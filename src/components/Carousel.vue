68bdb6bbd5aa8bddfd309f421936d937f30e625f
funtikcompany <73257723+funtikcompany@users.noreply.github.com>
2023-04-20 12:41:41 +0300

    Update Carousel.vue

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="slider">
-    <div class="slider-wrapper" :style="{ transform: 'translateX(' + position + 'px)' }">
-      <div @click="sliderSelect(index)" :class="{ 'active': select === index }" v-for="(item, index) in items"
-        :key="index" class="slider-item" :style="{ width: slideWidth + 'px' }">
-        <img :src="item.download_url" :alt="item.author">
+   <div class="div">
+    <div class="slider">
+      <div class="slider-wrapper" :style="{ transform: 'translateX(' + position + 'px)' }">
+        <div @click="sliderSelect(index, item)" v-for="(item, index) in items" :key="index" class="slider-item"
+          :style="{ width: slideWidth + 'px' }">
+          <img :src="item.download_url" :alt="item.author">
+        </div>
       </div>
+      <button class="slider-btn slider-btn-previous" @click="previous"><img src="../assets/left_arrow.svg"
+          alt=""></button>
+      <button class="slider-btn slider-btn-next" @click="next"><img src="../assets/right_arrow.svg" alt=""></button>
     </div>
-    <button class="slider-btn slider-btn-previous" @click="previous"><img src="../assets/left_arrow.svg" alt=""></button>
-    <button class="slider-btn slider-btn-next" @click="next"><img src="../assets/right_arrow.svg" alt=""></button>
-
+    <div class="src-block">
+      <a class="src-link" v-for="link in links" :href="link">{{ link }}</a>
+    </div>
   </div>
 </template>
 
@@ -27,6 +32,7 @@ export default {
       currentIndex: 0,
       slideWidth: 0,
       select: null,
+      links: [],
     }
   },
   created() {
@@ -48,8 +54,27 @@ export default {
       }
       this.position = -this.currentIndex * this.slideWidth;
     },
-    sliderSelect(index) {
-      this.select = index;
+     sliderSelect(index, item) {
+      if (this.select === index) {
+        this.select = null;
+      } else { 
+        this.select = index;
+
+      }
+      const sliderItem = event.currentTarget;
+
+      const isActive = sliderItem.classList.contains('active');
+
+      if (isActive) {
+        sliderItem.classList.remove('active');
+        const itemIndex = this.links.indexOf(item.download_url);
+        if (itemIndex !== -1) {
+          this.links.splice(itemIndex, 1);
+        }
+      } else {
+        sliderItem.classList.add('active');
+        this.links.push(item.download_url);
+      }
     },
     previous() {
       if (this.currentIndex === 0) {
